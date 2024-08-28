f8c44e3b1cd18dbfe41a0da9b72d1f10e9516614
David8Omar32 <L24550248@chihuahua2.tecnm.mx>
2024-08-28 16:17:53 -0600

    Add files via upload

---
+++ EVA1_4_Television/src/eva1_4_television/EVA1_4_Television.java
@@ -0,0 +1,29 @@
+package eva1_4_television;
+public class EVA1_4_Television {
+
+    public static void main(String[] args) {
+        //Datos (Tele)
+        String marcaTv,modeloTv,resolucionTv,conexionesTv,tecnologiaTv;
+        double precioTv;
+        int sizeTv;
+        
+        //Asignar valores (Television)
+        marcaTv = "Sony";
+        modeloTv = "55LA8600";
+        resolucionTv = "1920x1080 px";
+        conexionesTv = "Puertos HDMI, Cable óptico, LAN, Puerto USB, Cable coaxial";
+        tecnologiaTv = "LED";
+        precioTv = 2795;
+        sizeTv = 70;
+        
+        //Mostrando valores
+        System.out.println(marcaTv);
+        System.out.println(modeloTv);
+        System.out.println(resolucionTv);
+        System.out.println(conexionesTv);
+        System.out.println(tecnologiaTv);
+        System.out.println(precioTv);
+        System.out.println(sizeTv);
+    }
+    
+}
