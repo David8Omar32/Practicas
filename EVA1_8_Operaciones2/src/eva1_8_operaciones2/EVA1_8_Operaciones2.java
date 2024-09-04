475c3610a9ede84c27ff9f0e5d94d977883d4007
David8Omar32 <L24550248@chihuahua2.tecnm.mx>
2024-09-04 16:49:00 -0600

    Add files via upload

---
+++ EVA1_8_Operaciones2/src/eva1_8_operaciones2/EVA1_8_Operaciones2.java
@@ -0,0 +1,18 @@
+package eva1_8_operaciones2;
+public class EVA1_8_Operaciones2 {
+    public static void main(String[] args) {
+        //Datos definidos
+        String nombres = "David Omar";
+        String apellidos = "Gil Gómez";
+        
+        //Funciones
+        String nombreCom = nombres + " " + apellidos;
+        
+        //Datos impresos
+        System.out.println("El nombre es: " + nombres);
+        System.out.println("Los apellidos son: " +apellidos);
+        System.out.println("El nombre completo es: "+nombreCom);
+        
+    }
+    
+}
